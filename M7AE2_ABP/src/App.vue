d2cc7cec7e07f2e01c297e52fd1e9a91c78f51ab
Daniel Paredes <daniel.paredes@usach.cl>
2025-11-01 02:31:43 -0300

    vmos

---
+++ M7AE2_ABP/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import Footer from './components/Footer.vue';
+
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
+
+<template>
+  <header>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+  <footer>
+    <Footer />
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
